e6d4004fb8065374029c8d0d967b26ffcd13dd5d
Nithin-2005-dev <nithinkumar2005b@gmail.com>
2025-07-12 12:05:50 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Home from '../Page/Home'
+import Products from '../Page/Products'
+import Requests from '../Page/Requests'
+import Login from '../Page/Login'
+import Register from '../Page/Register'
+const App = () => {
+  return <Router>
+<Routes>
+  <Route path='/' element={<Home/>}/>
+  <Route path='/product' element={<Products/>}/>
+  <Route path='/requests' element={<Requests/>}/>
+  <Route path='/login' element={<Login/>}/>
+  <Route path='/register' element={<Register/>}/>
+</Routes>
+  </Router>
+}
+
+export default App
